1da02266f691a5e3a014c55450f4320795ac228b
julianjohannesen <github email address>
2018-12-10 21:24:42 -0500

    Updates

---
+++ precache-manifest.db677ca46c0abbd90de914145865dc0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc6a37d60432581f15be",
+    "url": "/static/js/main.dc6a37d6.chunk.js"
+  },
+  {
+    "revision": "08c83fe09ac1fc736ef8",
+    "url": "/static/js/1.08c83fe0.chunk.js"
+  },
+  {
+    "revision": "dc6a37d60432581f15be",
+    "url": "/static/css/main.dc6ebe5c.chunk.css"
+  },
+  {
+    "revision": "57ff446ad2a8349a5429ae0c9251039a",
+    "url": "/index.html"
+  }
+];
